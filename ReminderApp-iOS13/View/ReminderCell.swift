8ef842276fb5a3724a2af3257f02213e2c8d496e
Dandun Adi <dandun.adi@gmail.com>
2020-08-20 19:40:21 +0700

    customize TableViewCell and adding a header

---
+++ ReminderApp-iOS13/View/ReminderCell.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderCell.swift
+//  ReminderApp-iOS13
+//
+//  Created by DDD on 20/08/20.
+//  Copyright © 2020 Dandun Adi. All rights reserved.
+//
+
+import UIKit
+
+class ReminderCell: UITableViewCell {
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}
